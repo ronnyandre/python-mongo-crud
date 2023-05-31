9e7d5656039aae46bf0e2a40b78c8b81b936436a
Ronny-Andre Bendiksen <ronnyandre@gmail.com>
2023-05-31 12:31:25 +0200

    Updated routing in main file

--- app/main.py
+++ app/main.py
@@ -1,30 +1,25 @@
-from fastapi import FastAPI, APIRouter, status
+from app import note
+from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
-router = APIRouter()
 
-@router.get("/")
-def get_notes():
-    return "return a list of notes"
+api_version = "v1"
 
-@router.post("/", status_code=status.HTTP_201_CREATED)
-def create_note():
-    return "create note item"
+origins = [
+    "http://localhost:8000"
+]
 
-@router.patch("/{noteId}")
-def update_note(noteId: str):
-    return f"update note item with id {noteId}"
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"]
+)
 
-@router.get("/{noteId}")
-def get_note(noteId: str):
-    return f"get note item with id {noteId}"
+app.include_router(note.router, tags=["Notes"], prefix=f"/api/{api_version}/notes")
 
-@router.delete("/{noteId}")
-def delete_note(noteId: str):
-    return f"delete note item with id {noteId}"
-
-app.include_router(router, tags=["Notes"], prefix="/api/v1/notes")
-
-@app.get('/api/v1/healthcheck')
+@app.get(f"/api/{api_version}/healthcheck")
 def healthcheck():
     return {"message": "This is fine"}
