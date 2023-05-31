35f645f7b56162c58ca67f8cea10f8209aec90c2
Ronny-Andre Bendiksen <ronnyandre@gmail.com>
2023-05-31 12:23:12 +0200

    Implemented update functionality

--- app/note.py
+++ app/note.py
@@ -47,4 +47,22 @@ def create_note(payload: schemas.NoteBaseSchema):
     except:
         raise HTTPException(status_code=status.HTTP_409_CONFLICT,
                             detail=f"Note with title {payload.title} already exists")
-    
+
+# [...] Update a record
+@router.patch("/{noteId}", response_model=schemas.NoteResponse)
+def update_note(noteId: str, payload: schemas.UpdateNoteSchema):
+    if not ObjectId.is_valid(noteId):
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
+                            detail=f"Invalid id {noteId}")
+    updated_note = Note.find_one_and_update(
+        {"_id": ObjectId(noteId)},
+        {"$set": payload.dict(exclude_none=True)},
+        return_document=ReturnDocument.AFTER
+    )
+    if not updated_note:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
+                            detail=f"No note with ID {noteId}")
+    return {
+        "status": "success",
+        "note": noteEntity(update_note)
+    }
