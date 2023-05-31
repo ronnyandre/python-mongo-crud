3de312579b0a53d183c5af2aaaca2cd24ff3ce06
Ronny-Andre Bendiksen <ronnyandre@gmail.com>
2023-05-31 14:19:15 +0200

    Added some notes in the script

--- app/note.py
+++ app/note.py
@@ -8,10 +8,11 @@ from bson.objectid import ObjectId
 
 router = APIRouter()
 
+# Function to check if ID exists
 def check_if_id_exists(id: str):
      if not ObjectId.is_valid(id):
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST,
-                            detail=f"Invalid ID: {id}")
+                            detail=f"Invalid note ID {id}")
 
 # [...] Get 10 first records
 @router.get("/", response_model=schemas.ListNoteResponse)
@@ -38,6 +39,8 @@ def get_notes(limit: int = 10, page: int = 1, search: str = ""):
 # [...] Create a record
 @router.post("/", status_code=status.HTTP_201_CREATED, response_model=schemas.NoteResponse)
 def create_note(payload: schemas.NoteBaseSchema):
+
+    # Set create and update datetime
     payload.createdAt = datetime.utcnow()
     payload.updatedAt = payload.createdAt
 
@@ -57,34 +60,41 @@ def create_note(payload: schemas.NoteBaseSchema):
 @router.patch("/{noteId}", response_model=schemas.NoteResponse)
 def update_note(noteId: str, payload: schemas.UpdateNoteSchema):
 
+    # Check if ID exists
     check_if_id_exists(noteId)
 
+    # Convert to dictionary and update datetime
+    note_dict = payload.dict(exclude_none=True)
+    note_dict["updatedAt"] = datetime.utcnow()
+    
+    # Update in database
     updated_note = Note.find_one_and_update(
         {"_id": ObjectId(noteId)},
-        {"$set": payload.dict(exclude_none=True)},
+        {"$set": note_dict},
         return_document=ReturnDocument.AFTER
     )
 
     if not updated_note:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"No note with ID {noteId}")
+                            detail=f"Note with ID {noteId} does not exist")
     
     return {
         "status": "success",
-        "note": noteEntity(update_note)
+        "note": noteEntity(updated_note)
     }
 
 # [...] Get a single record
 @router.get("/{noteId}", response_model=schemas.NoteResponse)
 def get_note(noteId: str):
 
+    # Check if ID exists
     check_if_id_exists(noteId)
 
     note = Note.find_one({"_id": ObjectId(noteId)})
 
     if not note:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"No note with this ID {noteId}")
+                            detail=f"Note with ID {noteId} does not exist")
     
     return {
         "status": "success",
@@ -95,12 +105,13 @@ def get_note(noteId: str):
 @router.delete("/{noteId}")
 def delete_note(noteId: str):
     
+    # Check if ID exists
     check_if_id_exists(noteId)
 
     note = Note.find_one_and_delete({"_id": ObjectId(noteId)})
 
     if not note:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND,
-                            detail=f"No note with ID {noteId}")
+                            detail=f"Note with ID {noteId} does not exist")
     
     return Response(status_code=status.HTTP_204_NO_CONTENT)
